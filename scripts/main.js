7f880ccf57ab58190407575e6983f8c8d3803beb
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-08-26 11:36:19 -0500

    Practice 1

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var title = document.querySelector(".article__header");
+title.textContent = "Welcome to Heidi's Mug blog";
+
+var article_header = document.querySelectorAll(".article__header");
+article_header.classList + "!important";
+
+var dashed = document.querySelector(".dashed");
+dashed.classList.remove("dashed");
+
+var articlefooter = document.querySelector(".article__footer");
+articlefooter.classList.add("goldenrod");
+
+
